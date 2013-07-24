299e94fe3731dc5839358886bf72ff8c37deddba
Shibly <shibly.phy@gmail.com>
2013-07-25 01:11:54 +0600

    Done. You can indent the json by supply true as second parameter to the method.

--- MyJSON.php
+++ MyJSON.php
@@ -6,12 +6,24 @@ class MySql_To_Json
     private $connection;
     public $errors = array();
 
+    /**
+     * @param $db_server
+     * @param $db_username
+     * @param $db_password
+     * @param $db_name
+     */
+
     public function __construct($db_server, $db_username, $db_password, $db_name)
     {
         $this->connection = new PDO("mysql:host=$db_server;dbname=$db_name", $db_username, $db_password);
     }
 
-    public function MySQLtoJSON($query)
+    /**
+     * @param $query
+     * @param bool $indented
+     * @return bool|string
+     */
+    public function MySQLtoJSON($query, $indented = false)
     {
         $query = $this->connection->query($query) or die("Unable to execute the query");
         if (!$numFields = $query->columnCount()) {
@@ -19,32 +31,51 @@ class MySql_To_Json
             return false;
         }
 
-        $fields = array();
-        $colNames = array();
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
 
-        for ($i = 0; $i < $numFields; $i++) {
-            $fields[$i] = $query->getColumnMeta($i);
-            foreach ($fields as $field) {
-                $colNames[] = $field['name'];
-            }
-        }
 
-        if (!$numRows = $query->rowCount()) {
-            $this->errors[] = "Unable to get the number of rows";
-            return false;
-        }
-
-        // return $numRows;
-        $result = array();
-        for ($i = 0; $i < $numFields; $i++) {
-            $result[$i] = array();
-            for ($j = 0; $j < count($field); $j++) {
-                return $result[$i][$field[$j]] = $query->fetch($i);
-            }
-        }
         $json = json_encode($result);
-        return $json;
+        if ($indented == false)
+            return $json;
 
+        $result = '';
+        $pos = 0;
+        $previous = '';
+        $outQuotes = true;
+        for ($i = 0; $i <= strlen($json); $i++) {
+
+            // Next char
+            $char = substr($json, $i, 1);
+
+            // Inside quote?
+            if ($char == '"' && $previous != '\\') {
+                $outQuotes = !$outQuotes;
+
+                // End of element? New line and indent
+            } elseif (($char == '}' || $char == ']') && $outQuotes) {
+                $result .= "\n";
+                $pos--;
+                for ($j = 0; $j < $pos; $j++)
+                    $result .= '	';
+            }
+
+            // Add the character to the result string.
+            $result .= $char;
+
+            // Beginning of element? New line and indent
+            if (($char == ',' || $char == '{' || $char == '[') && $outQuotes) {
+                $result .= "\n";
+                if ($char == '{' || $char == '[')
+                    $pos++;
+
+                for ($j = 0; $j < $pos; $j++)
+                    $result .= '	';
+            }
+
+            $previous = $char;
+        }
+
+        return $result;
     }
 
 }
@@ -53,7 +84,7 @@ class MySql_To_Json
 
 
 $db = new MySql_To_Json('localhost', 'root', 'rivergod', 'drupal');
-$res = $db->MySQLtoJSON('Select * from users');
+$res = $db->MySQLtoJSON('Select * from users', true);
 var_dump($res);
 
 ?>
